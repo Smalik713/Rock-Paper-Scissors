23a7da040f7a5cb7f1ae49d985f6bb51702a6800
Sami Malik <hssnmlk321@gmail.com>
2021-06-16 17:05:20 -0500

    second commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const charmander_div = document.getElementById("charmanderBtn");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
